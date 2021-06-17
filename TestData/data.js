73a00dad82716005bc91a6e761b66707085b6c44
Aswinamar <72555684+Aswinamar@users.noreply.github.com>
2021-06-17 23:59:23 +0530

    Update data.js
    Update data.js

--- TestData/data.js
+++ TestData/data.js
@@ -4,9 +4,7 @@ module.exports = {
     StringValues: {
         DHeader   : 'A million more | Volvo Cars - International',
         DHomeH2       : 'Ideas that change the world are often the most controversial.',
-        icoChartDesign  : '//button[@class="md-icon-button chart-design mat-raised-button"]',
-        icoReportDesign : '//button[@class="md-icon-button report-design mat-raised-button"]',
-        lblTitle        : '//h2',
+       
     },
     
     ArrayValues: {
